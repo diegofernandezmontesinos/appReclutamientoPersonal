a3d1418e1dfa86aa40031018d578f689e0e05713
dfmontesinos <dfmontesinos@gmail.com>
2023-08-03 21:05:43 +0200

    Primer commit del project

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import { MiRouter } from './router/MiRouter.js'
+import { MiRouter } from './router/MiRouter.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
