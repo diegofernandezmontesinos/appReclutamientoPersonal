6d973ffc3681081ae11fb6c787467f0fd1d393ed
dfmontesinos <dfmontesinos@gmail.com>
2023-08-03 21:24:56 +0200

    creacion candidato y recuperacion api

--- src/componentes/Listado.jsx
+++ src/componentes/Listado.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Listado = () => {
+    const [candidatos, setCandidatos] = useState([])
+
+    useEffect(()=>{
+        fetch("https://randomuser.me/api/?results=6")
+        .then((response) => response.json())
+        .then((datos) => setCandidatos(datos.results))
+    }, [])
     return (
-        <div>Listado</div>
+        <>
+            {candidatos.map((valor,index)=>
+            <div>
+                {valor.name.first}
+            </div>)}
+        </>
     )
 }
 
